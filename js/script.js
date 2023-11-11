8c0e343341d893b133fdc239c5617de6f6843393
dganesh22 <dganesh2022@gmail.com>
2023-11-11 14:08:56 +0530

    completed home page

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   search.classList.remove('active');
+}
+
+let search = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   search.classList.toggle('active');
+   profile.classList.remove('active');
+}
